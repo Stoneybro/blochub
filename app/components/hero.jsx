653bdd5f88f158cdf2f8ed3693cdcf1c4cb3a9c4
Zion livingstone <zionlivingstone4@gmail.com>
2023-07-18 22:08:14 +0100

    v

--- app/components/hero.jsx
+++ app/components/hero.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import hero from '../../public/Hero.png'
+import hero from '../../public/hero.png'
 import Image from 'next/image'
 const Hero = () => {
   return (
     <div className='relative'>
 
-    <Image src={hero} />
+    <Image src={hero}  />
     <div className="hidden lg:block absolute top-[75.5%] font-semibold text-[35px] text-white px-20 py-4 bg-light border-b-4 border-pink">Empowering Minds, Shaping Futures</div>
   </div>
   )
