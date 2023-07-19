7c30d83b760a0982cc5b8a78e0bc6f49f35b924d
Zion livingstone <zionlivingstone4@gmail.com>
2023-07-19 14:09:35 +0100

    x

--- app/components/nav.jsx
+++ app/components/nav.jsx
@@ -14,17 +14,36 @@ const Nav = () => {
   const [school,setSchool]=useState(false)
   const [nav,setNav]=useState(false)
   const [toggle,setToggle]=useState(false)
-
+  const [mobilenav,setMobilenav]=useState({school:true,admission:true})
 
 
 
   return (
     <nav className=' sticky top-0 w-full bg-primary  h-[82px] z-10 pb-4  '>
 
-      <div className="flex  items-center mx-auto max-w-[1130px] relative">
+      <div className="flex  items-center mx-auto max-w-[1200px] relative">
       <div className='mr-auto '><Link href={'/'} className='inline'><Image src={Evaton}  /></Link></div>
       <div className="lg:hidden flex mr-4 ml-1 z-10" onClick={()=>setToggle(!toggle)}>{toggle?<BiX size={45} color='rgba(25, 41, 106, 1)' />:<BiMenu size={45} color='rgba(25, 41, 106, 1)' />}</div>
-     {toggle&& <div className="bg-white h-screen w-screen fixed overflow-hidden top-0 pointer-events-none left-0 "></div>}
+     {toggle&& <div className="bg-white h-screen w-screen fixed overflow-hidden top-0  left-0  flex items-start justify-center">
+     <div className="text-secondary flex flex-col justify-center items-center gap-8 mt-20 text-xl font-semibold">
+        <Link href={'#'} className="text-secondary">HOME</Link>
+        <Link href={'#'} className="">ABOUT </Link>
+        <button onClick={()=>setMobilenav({...mobilenav,school:!mobilenav.school})} className={`flex items-center  ${!mobilenav.school && 'text-pink'}`}>SCHOOLS<RxCaretDown size={32} /></button>
+        <div className={`${mobilenav.school&&'hidden'} flex flex-col gap-8 items-center`}>
+         <Link href={'#'} className="">Nursery and Primary School</Link>
+         <Link href={'#'} className="">Secondary School</Link>
+        </div>
+        <button onClick={()=>setMobilenav({...mobilenav,admission:!mobilenav.admission})} className="flex items-center"><Link href={'#'} className={`flex items-center  ${!mobilenav.admission && 'text-pink'}`}>ADMISSIONS <RxCaretDown size={32} /></Link></button>
+        <div className={`${mobilenav.admission&&'hidden'} flex flex-col gap-8 items-center`}>
+         <Link href={'#'} className="">Admission Procedure</Link>
+         <Link href={'#'} className="">Boarding</Link>
+         <Link href={'#'} className="">School Fees</Link>
+         <Link href={'#'} className="">Scholarships</Link>
+        </div>
+        <Link href={'#'} className="">ACADEMICS </Link>
+        <Link href={'#'} className="">CONTACT US</Link>
+      </div>
+      </div>}
       <div className=" hidden lg:flex justify-between items-center gap-8  h-[80px]   mb-2 font-bold text-sm">
       <div >
 
