a4fcd5c851fe63131942b6fe5236f60a70989dfa
Zion livingstone <zionlivingstone4@gmail.com>
2023-08-05 22:35:12 +0100

    x

--- app/Nav.jsx
+++ app/Nav.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-import logo from '../../public/logo.svg'
+import logo from '../public/logo.svg'
 import {BiMenu} from 'react-icons/bi'
 const Nav = () => {
   return (
